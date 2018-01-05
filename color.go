0c83f2e2c01bd38be5665409d56a68daec894110
Johan Vester <johan.vester@gmail.com>
2018-01-05 23:34:09 +0100

    Implement Hex and RGB color profiles

--- color.go
+++ color.go
@@ -1,58 +1,101 @@
 package color
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
 
-// Hex is a color represented as single numeric value, usually written out
-// in its hexadecimal form.
+// Color is a HTML color represented as single int value.
+type Color uint
+
+func (c Color) String() string {
+	return c.Hex().String()
+}
+
+func C(v int) (Color, error) {
+	if v > int(CMax) {
+		return 0, fmt.Errorf("%x exceeds color max value (%x)", v, CMax)
+	}
+
+	if v < int(CMin) {
+		return 0, fmt.Errorf("invalid color value %x, color value must be positive", v)
+	}
+
+	return Color(v), nil
+}
+
+// CMin is the lowest color value.
+const CMin Color = 0x0
+
+// CMax is the highest color value.
+const CMax Color = 0xffffff
+
+// Hex is a color represented as a hexadecimal string, prefixed with
+// a single # sign.
+//
+// Hex only represents full hexadecimal color triplets (i.e. not colors on the
+// shorthand hexadecimal form).
 type Hex struct {
-	// v is the color value from 0 to 16 777 215.
-	v int
+	// v is the hexadecimal color string.
+	v string
 }
 
-func (h *Hex) Hex() Hex {
-	return *h
+func (h *Hex) Color() Color {
+	v := strings.TrimPrefix(h.v, "#")
+	c, err := strconv.ParseInt(v, 16, 32)
+	if err != nil {
+		panic(err)
+	}
+	return Color(c)
 }
 
-func (h *Hex) RGB() RGB {
-	r := (h.v >> 16) & 255
-	g := (h.v >> 8) & 255
-	b := h.v & 255
-
-	return RGB{uint8(r), uint8(g), uint8(b)}
+func (h Hex) String() string {
+	return fmt.Sprintf("#%s", h.v)
 }
 
-func (h *Hex) String() string {
-	return fmt.Sprintf("%x", h.v)
+func (c Color) Hex() Hex {
+	rgb := c.RGB()
+	h := fmt.Sprintf("%02x%02x%02x", uint8(rgb.r), rgb.g, rgb.g)
+
+	return Hex{h}
+}
+
+func (c Color) RGB() RGB {
+	r := (c >> 16) & 255
+	g := (c >> 8) & 255
+	b := c & 255
+
+	return RGB{uint(r), uint(g), uint(b)}
 }
 
 // RGB is a color consisting of three 8 bit channels; red, green, and blue.
 type RGB struct {
-	r uint8
-	g uint8
-	b uint8
+	r uint
+	g uint
+	b uint
 }
 
-func (rgb *RGB) RGB() RGB {
-	return *rgb
+func (rgb *RGB) Color() Color {
+	v := (rgb.r << 16) | (rgb.g << 8) | rgb.b
+
+	return Color(v)
 }
 
-func (rgb *RGB) Hex() Hex {
-	v := rgb.b | (rgb.g << 8) | (rgb.r << 16)
-
-	return Hex{int(v)}
+func (rgb RGB) String() string {
+	return fmt.Sprintf("rgb(%d, %d, %d)", rgb.r, rgb.g, rgb.b)
 }
 
 // RGBA is an RGB color with an additional alpha (transparency) channel.
 type RGBA struct {
 	RGB
-	// a is the color alpha channel, where
-	// 0 is completely transparent
-	// 1 is completely opaque
+	// a is the color alpha channel between 0.0 and 1.0, where
+	// 0.0 is completely transparent
+	// 1.0 is completely opaque
 	a float32
 }
 
-type Color interface {
-	Hex() Hex
-	RGB() RGB
+type Format interface {
+	Color() int
 }
 
---
+++ color_test.go
@@ -0,0 +1,88 @@
+package color
+
+import "testing"
+
+func TestColor_String(t *testing.T) {
+	cases := []struct{
+		c Color
+		exp string
+	}{
+		{
+			c: CMax,
+			exp: "#ffffff",
+		},
+		{
+			c: CMin,
+			exp: "#000000",
+		},
+		{
+			c: Color(1052688),
+			exp: "#101010",
+		},
+	}
+
+	for _, c := range cases {
+		act := c.c.String()
+
+		if act != c.exp {
+			t.Errorf("Expected %v.String() to return %q, %q given", c.c, c.exp, act)
+		}
+	}
+}
+
+func TestHex_Color(t *testing.T) {
+	cases := []struct{
+		c Hex
+		exp Color
+	}{
+		{
+			c: Hex{"#ffffff"},
+			exp: CMax,
+		},
+		{
+			c: Hex{"#000000"},
+			exp: CMin,
+		},
+		{
+			c: Hex{"#101010"},
+			exp: Color(1052688),
+		},
+	}
+
+	for _, c := range cases {
+		act := c.c.Color()
+
+		if act != c.exp {
+			t.Errorf("Expected %v.Color() to be %v, %v given", c.c, c.exp, act)
+		}
+	}
+}
+
+func TestRGB_Color(t *testing.T) {
+	cases := []struct{
+		c RGB
+		exp Color
+	}{
+		{
+			c: RGB{255, 255, 255},
+			exp: CMax,
+		},
+		{
+			c: RGB{0, 0, 0},
+			exp: CMin,
+		},
+		{
+			c: RGB{16, 16, 16},
+			exp: Color(1052688),
+		},
+	}
+
+	for _, c := range cases {
+		act := c.c.Color()
+
+		if act != c.exp {
+			t.Errorf("Expected %v.Color() to be %v, %v given", c.c, c.exp, act)
+		}
+	}
+}
+
